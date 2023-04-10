620605246f43d269b873c4cf337be55733908c2b
InaUA <inanurbaeva@gmail.com>
2023-04-10 14:19:09 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,3 @@
-
 const bodyChangeColor = document.querySelector('body');
 const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]');
@@ -6,21 +5,19 @@ startButton.addEventListener('click', startChangeColor);
 stopButton.addEventListener('click', stopChangeColor);
 let intervalId = null;
 
+function startChangeColor() {
+  startButton.disabled = true;
+  intervalId = setInterval(() => {
+    let randomColor = getRandomHexColor();
+    bodyChangeColor.style.background = randomColor;
+  }, 1000);
+}
+
+function stopChangeColor() {
+  startButton.disabled = false;
+  clearInterval(intervalId);
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
-
-
-function startChangeColor() {
-   startButton.disabled = true;
-    intervalId = setInterval(() => {
-        let randomColor = getRandomHexColor();
-        bodyChangeColor.style.background = randomColor;
-    }, 1000);
-};
-
-function stopChangeColor() {
-    startButton.disabled = false;
-    clearInterval(intervalId);
-};
